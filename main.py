aec0a3717eac79ad86ea4d313c2f07a480aa41d0
bourskov83 <mail@bourskov.dk>
2023-12-27 20:10:52 +0100

    .

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ import ujson
 from mqtt_as import MQTTClient
 from mqtt_as import config as mqtt_config
 from PID import PID
+import tm1637
 
 
 
 # Pin definitions
 ONEWIRE_PIN = 32
-COOLING_PIN = 19
-HEATING_PIN = 2
+HEATING_PIN = 4
+displayPinClk = 14
+displayPinDio = 27
 
 # Initialize Watchdog
 wdt = machine.WDT(timeout=5000)
@@ -27,17 +29,19 @@ with open('setpoint.json') as f:
     setpoint = ujson.loads(f.read())
 
 
-cooling = machine.Pin(COOLING_PIN, machine.Pin.OUT)
-heating = machine.Pin(HEATING_PIN, machine.Pin.OUT)
+#heating = machine.Pin(HEATING_PIN, machine.Pin.OUT)
+heating = machine.PWM(HEATING_PIN)
+heating.freq(1)
+displayMlt = tm1637.TM1637(clk=machine.Pin(displayPinClk),dio=machine.Pin(displayPinDio))
 
     
-mqtt_config['server'] = config['wifi']['mqtt_server']
-mqtt_config['ssid'] = config['wifi']['ssid']
-mqtt_config['wifi_pw'] = config['wifi']['wifi_pw']
+#mqtt_config['server'] = config['wifi']['mqtt_server']
+#mqtt_config['ssid'] = config['wifi']['ssid']
+#mqtt_config['wifi_pw'] = config['wifi']['wifi_pw']
 
 
-MQTTClient.DEBUG = True # type: ignore
-mqttclient = MQTTClient(mqtt_config)
+#MQTTClient.DEBUG = True # type: ignore
+#mqttclient = MQTTClient(mqtt_config)
 
 
 owPin = machine.Pin(ONEWIRE_PIN)
@@ -49,11 +53,13 @@ for i in temp_sensors:
 
 print(temp_sensors)
 
+mltPid = PID(1,3,0.2, setpoint=50, scale='ms')
+mltPid.output_limits=(0,1023)
 
-async def mqtt_connect(client):
-    await client.connect()
+#async def mqtt_connect(client):
+#    await client.connect()
 
-    await client.publish("fermcontrol/ipaddr", str(client._sta_if.ifconfig()[0]),qos=1)
+#    await client.publish("fermcontrol/ipaddr", str(client._sta_if.ifconfig()[0]),qos=1)
 
 
 async def temp_sensor_read():
@@ -69,95 +75,108 @@ async def temp_sensor_read():
                 sensor['value'] = "NA"
              
         
-async def mqtt_update(client):
-    while True:
-        await uasyncio.sleep(10)
+#async def mqtt_update(client):
+#    while True:
+#        await uasyncio.sleep(10)
 
-        for i in temp_sensors:
-            if i['value'] != "NA":
-                await client.publish(f"fermcontrol/{i['name']}/temperature/state",str(round(i['value'],1)),qos=1)
-        await client.publish("fermcontrol/cooling/state",str(cooling.value()),qos=1)
-        await client.publish("fermcontrol/heating/state",str(heating.value()),qos=1)
-        await client.publish("fermcontrol/chambersetpoint/state",setpoint['chamberSetpoint'],qos=1)
-        await client.publish("fermcontrol/vesselsetpoint/state",setpoint['vesselSetpoint'],qos=1)
+#        for i in temp_sensors:
+#            if i['value'] != "NA":
+#                await client.publish(f"fermcontrol/{i['name']}/temperature/state",str(round(i['value'],1)),qos=1)
+#        await client.publish("fermcontrol/cooling/state",str(cooling.value()),qos=1)
+#        await client.publish("fermcontrol/heating/state",str(heating.value()),qos=1)
+#        await client.publish("fermcontrol/chambersetpoint/state",setpoint['chamberSetpoint'],qos=1)
+#        await client.publish("fermcontrol/vesselsetpoint/state",setpoint['vesselSetpoint'],qos=1)
         
-
-
-app = Microdot()
-
-@app.route('/')
-async def home(request):
-    return send_file('/static/index.html')
-
-
-@app.route('/api/values', methods=['GET'])
-async def values(request):
-    payload = {item['name']: item['value'] for item in temp_sensors}
-    payload['coolingState'] = bool(cooling.value())
-    payload['heatingState'] = bool(heating.value())
-
-    return Response(headers={'Content-Type' : 'application/json','Access-Control-Allow-Origin':'*'}, body=ujson.dumps(payload))
-
-
-
-@app.route('/api/setpoint', methods=['GET'])
-async def getSetpoint(request):
-    if 'action' in request.args:
-        if request.args['action'] == 'update':
-            setpoint['chamberSetpoint'] = request.args['chamber']
-            setpoint['vesselSetpoint'] = request.args['vessel']
-            with open('setpoint.json', "w") as f:
-                ujson.dump(setpoint, f)
-
-    payload = setpoint
-    return Response(headers={'Content-Type' : 'application/json','Access-Control-Allow-Origin':'*'}, body=ujson.dumps(payload))
-
-
-async def start_server():
-    await app.start_server(port=80)
-
-
-
-async def showResult():
-    while True:
-        await uasyncio.sleep(1)
-        result = {'sensorReadings':temp_sensors, "cooling":cooling.value(), "heating":heating.value(), "setpoint":setpoint['chamberSetpoint']}
-        print(result)
+#
+#
+#app = Microdot()
+#
+#@app.route('/')
+#async def home(request):
+#    return send_file('/static/index.html')
+#
+#
+#@app.route('/api/values', methods=['GET'])
+#async def values(request):
+#    payload = {item['name']: item['value'] for item in temp_sensors}
+#    payload['coolingState'] = bool(cooling.value())
+#    payload['heatingState'] = bool(heating.value())
+#
+#    return Response(headers={'Content-Type' : 'application/json','Access-Control-Allow-Origin':'*'}, body=ujson.dumps(payload))
+#
+#
+#
+#@app.route('/api/setpoint', methods=['GET'])
+#async def getSetpoint(request):
+#    if 'action' in request.args:
+#        if request.args['action'] == 'update':
+#            setpoint['chamberSetpoint'] = request.args['chamber']
+#            setpoint['vesselSetpoint'] = request.args['vessel']
+#            with open('setpoint.json', "w") as f:
+#                ujson.dump(setpoint, f)
+#
+#    payload = setpoint
+#    return Response(headers={'Content-Type' : 'application/json','Access-Control-Allow-Origin':'*'}, body=ujson.dumps(payload))
+#
+#
+#async def start_server():
+#    await app.start_server(port=80)
+#
+#
+#
+#async def showResult():
+#    while True:
+#        await uasyncio.sleep(1)
+#        result = {'sensorReadings':temp_sensors, "cooling":cooling.value(), "heating":heating.value(), "setpoint":setpoint['chamberSetpoint']}
+#        print(result)
 async def updateOutput():
     while True:
-        await uasyncio.sleep_ms(1000)
+        await uasyncio.sleep_ms(500)
+        print(temp_sensors)
+        #print(f"MLT HEAT: {heating.value()}")
+        mlt_temp = next((sensor['value'] for sensor in temp_sensors if sensor['name'] == 'mlt'))
+        if mlt_temp != "NA":
+            control=mltPid(mlt_temp)
+            print(control)
+            heating.duty(int(control))
+            displayMlt.temperature(int(mlt_temp))
 
-        coolStart = int(setpoint['chamberSetpoint']) + int(config['cooling']['hysteresis']) / 2 
-        coolStop = int(setpoint['chamberSetpoint']) - int(config['cooling']['hysteresis']) / 2 + 0.3
-        heatStart = int(setpoint['chamberSetpoint']) - int(config['heating']['hysteresis']) / 2 
-        heatStop = int(setpoint['chamberSetpoint']) + int(config['heating']['hysteresis']) / 2 - 0.3
-        
-        chamber_value = next((sensor['value'] for sensor in temp_sensors if sensor['name'] == 'chamber'))
-        
-        if chamber_value >= coolStart:
-            if not cooling.value():
-                cooling.on()
-        elif chamber_value <= coolStop:
-            if cooling.value():
-               cooling.off()
+#        if mlt_temp <= 40:
+#            heating.on()
+#        else:
+#            heating.off()
 
-        if chamber_value <= heatStart:
-            if not heating.value():
-                heating.on()
-        elif chamber_value >= heatStop:
-            if heating.value():
-               heating.off()
+
+        
+
+#        coolStart = int(setpoint['chamberSetpoint']) + int(config['cooling']['hysteresis']) / 2 
+#        coolStop = int(setpoint['chamberSetpoint']) - int(config['cooling']['hysteresis']) / 2 + 0.3
+#        heatStart = int(setpoint['chamberSetpoint']) - int(config['heating']['hysteresis']) / 2 
+#        heatStop = int(setpoint['chamberSetpoint']) + int(config['heating']['hysteresis']) / 2 - 0.3
+#        
+#        chamber_value = next((sensor['value'] for sensor in temp_sensors if sensor['name'] == 'chamber'))
+#        
+#        if chamber_value >= coolStart:
+#            if not cooling.value():
+#                cooling.on()
+#        elif chamber_value <= coolStop:
+#            if cooling.value():
+ #              cooling.off()
+#
+ #       if chamber_value <= heatStart:
+ #           if not heating.value():
+ #               heating.on()
+ #       elif chamber_value >= heatStop:
+ #           if heating.value():
+ #              heating.off()
 
         wdt.feed()
 
 loop = uasyncio.get_event_loop()    
 loop.create_task(temp_sensor_read())
-loop.create_task(start_server())
-loop.create_task(mqtt_connect(mqttclient))
-loop.create_task(mqtt_update(mqttclient))
+#loop.create_task(start_server())
+#loop.create_task(mqtt_connect(mqttclient))
+#loop.create_task(mqtt_update(mqttclient))
 #loop.create_task(showResult())
 loop.create_task(updateOutput())
 loop.run_forever()
-
-
-
